c3b88c6e4b3d3a0d5f58b60c4a2cf60c65e5f16e
SathiyaSri <sathiyasri1609@example.com>
2025-06-16 19:37:06 +0530

    Cloudinary integration and deploy fixes

--- recipesite/settings.py
+++ recipesite/settings.py
@@ -99,8 +99,6 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Required for collectstatic in production
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
